75b58110f5b7e77b0449c9cc2e6e787eef1a4671
rkessal <kessal.rayhan25@gmail.com>
2022-08-10 16:55:55 +0200

    added number of rolls, current time and best time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Die from "./components/Die"
 import React from "react"
 import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
+import { useStopwatch } from 'react-timer-hook';
 
 function App() {
 
@@ -14,15 +15,42 @@ function App() {
   
   const [dice, setDice] = React.useState(allNewDice())
   const [tenzies, setTenzies] = React.useState(false)
+  const [rolls, setRolls] = React.useState(0)
+  const [time, setTime] = React.useState({min: 999, sec: 999})
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true })
+  const currentTime = getTotalTime(minutes, seconds)
+  localStorage.setItem("time", getTotalTime(time.min, time.sec))
+
+ 
 
   React.useEffect(() => {
     const firstValue = dice[0].value
     const allHeld = dice.every(die => die.isHeld)
     const allSame = dice.every(die => die.value === firstValue)
 
+    
     if (allHeld && allSame) {
+      pause()
+      setTime(prevTime => {
+        if (currentTime <= localStorage.getItem("time")) {
+          return {
+            min: minutes,
+            sec: seconds
+          }
+        } else {
+          return prevTime
+        }
+      })
       setTenzies(true)
-      console.log("You won!")
     }
   }, [dice])
   
@@ -47,11 +75,19 @@ function App() {
     return diceArray
   }
 
+  function getTotalTime(minutes, seconds) {
+    return minutes * 60 + seconds
+  }
+
   function rollDice() {
 
     if (tenzies) {
+      reset()
       setDice(allNewDice())
+      setRolls(0)
       setTenzies(false)
+    } else {
+      setRolls(rolls + 1)
     }
 
     setDice(prevDice => prevDice.map(die => {
@@ -67,6 +103,7 @@ function App() {
       }))
     }
   }
+
   
 
 
@@ -76,6 +113,12 @@ function App() {
       {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <br/>
+        <div className="scoreboard">
+          <div className="rolls">Rolls: {rolls}</div>
+          <div className="stopwatch">Time: {`${minutes}:${seconds}`}</div>
+          <div className="bestTime">Best Time: {time.min === 999 ? "--:--" : time.min + ":" + time.sec}</div>
+        </div>
       <div className="dice-container">
         {dice.map((die) => <Die 
           key={die.id} 
